45e4eed597d5dce90aaa3be9f5fa990af173c4c0
lemin194 <leminhduc190204@gmail.com>
2023-09-23 14:40:46 +0700

    Minor changes

--- src/main/java/Dictionary/Services/TestAPI.java
+++ src/main/java/Dictionary/Services/TestAPI.java
@@ -12,7 +12,8 @@ public class TestAPI {
 
     public static void TestTranslate() {
         System.out.println("Begin connecting");
-        String ret = Translation.TranslateText("\n \t    ",
+        String ret = Translation.TranslateText(
+                "",
                 "en", "vi");
         System.out.println("Translated: \n" + ret);
     }
