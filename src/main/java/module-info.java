d845f973efb251c4b6ce39c22bee28e9d42308ef
lemin194 <leminhduc190204@gmail.com>
2023-09-23 12:30:50 +0700

    Add Translation Service

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.dictionary.dictionaryplus {
     requires javafx.controls;
     requires javafx.fxml;
     requires jlayer;
+    requires org.json;
 
 
     opens Dictionary.Views to javafx.fxml;
