70f519115b14fedba5747b847bc825facb34d46f
Tăng Vĩnh Hà <121566444+hachane@users.noreply.github.com>
2023-10-20 21:07:59 +0700

    Changes on Wiki Controller.

--- src/main/java/dictionary/controllers/MainController.java
+++ src/main/java/dictionary/controllers/MainController.java
@@ -22,7 +22,7 @@ public class MainController implements Initializable{
     @FXML
     private Label logo = new Label();
     @FXML
-    private Button searchBtn, translateBtn, reviewBtn, wikiBtn, exitBtn;
+    private Button searchBtn, translateBtn, reviewBtn, editBtn, exitBtn, wikiBtn;
 
     @FXML
     public AnchorPane container = new AnchorPane();
@@ -35,7 +35,10 @@ public class MainController implements Initializable{
 
         translateBtn.setOnAction(event -> showComponent("/view/Translate.fxml"));
 
+        editBtn.setOnAction(event -> showComponent("/view/Edit.fxml"));
+
         wikiBtn.setOnAction(event -> showComponent("/view/Wiki.fxml"));
+
 
         showComponent("/view/Search.fxml");
 
--- src/main/java/dictionary/controllers/WikiController.java
+++ src/main/java/dictionary/controllers/WikiController.java
@@ -1,12 +1,21 @@
 package dictionary.controllers;
 
 import dictionary.services.WikFetchService;
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.web.*;
 
-public class WikiController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent.*;
+public class WikiController implements Initializable {
     @FXML
     private TextField searchBox = new TextField();
     @FXML
@@ -14,11 +23,83 @@ public class WikiController {
     @FXML
     private Button search;
     @FXML
+    private WebView webView;
+    private WebHistory history;
+    @FXML
+            private ChoiceBox<String> historyURL = new ChoiceBox<>();
+    WebEngine engine = null;
+    @FXML
+    private Button load = new Button();
+    @FXML
+    private Button refresh = new Button();
+    @FXML
+    private Button zoomIn = new Button();
+    @FXML
+    private Button zoomOut = new Button();
+    @FXML
+    private Button historyBtn = new Button();
+    @FXML
+    private Button back = new Button();
+    @FXML
+    private Button forward = new Button();
+    private double webZoom;
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        engine = webView.getEngine();
+        webZoom = 1;
+    }
+
+    @FXML
+    public void loadPage(ActionEvent event) {
+        engine.load("https://en.wikipedia.org/wiki/" + searchBox.getText());
+
+    }
+    @FXML
     public void handleSearch() {
         String word = searchBox.getText();
         if (word.isBlank() || word.isEmpty()) renderedText.setText("");
         renderedText.setWrapText(true);
         WikFetchService wfService = new WikFetchService();
         renderedText.setText(wfService.search(word));
+    }
+    @FXML
+    public void load() {
+        engine.load("https://en.wikipedia.org/wiki/" + searchBox.getText());
+    }
+
+    @FXML
+    public void refreshPage() {
+        engine.reload();
+    }
+    @FXML
+    public void zoomIn() {
+        webZoom+=0.25;
+        webView.setZoom(webZoom);
+    }
+    @FXML
+    public void zoomOut() {
+        webZoom-=0.75;
+        webView.setZoom(webZoom);
+    }
+
+    @FXML
+    public void displayHistory() {
+        history = engine.getHistory();
+        ObservableList<WebHistory.Entry> entries = history.getEntries();
+        for(WebHistory.Entry entry : entries) {
+            System.out.println(entry.getUrl().getClass());
+        }
+    }
+    @FXML
+    public void goBack() {
+        history = engine.getHistory();
+        ObservableList<WebHistory.Entry> entries = history.getEntries();
+        history.go(-1);
+    }
+    @FXML
+    public void goForward() {
+        history = engine.getHistory();
+        ObservableList<WebHistory.Entry> entries = history.getEntries();
+        history.go(1);
     }
 }
