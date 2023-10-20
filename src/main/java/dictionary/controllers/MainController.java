ab040359481b0f3edafe7ea14e0e34c9f6f07323
Tăng Vĩnh Hà <121566444+hachane@users.noreply.github.com>
2023-10-20 21:07:59 +0700

    Changes on Wiki Controller.

--- src/main/java/dictionary/controllers/MainController.java
+++ src/main/java/dictionary/controllers/MainController.java
@@ -22,7 +22,7 @@ public class MainController implements Initializable{
     @FXML
     private Label logo = new Label();
     @FXML
-    private Button searchBtn, translateBtn, reviewBtn, wikiBtn, exitBtn;
+    private Button searchBtn, translateBtn, reviewBtn, editBtn, exitBtn;
 
     @FXML
     public AnchorPane container = new AnchorPane();
@@ -35,7 +35,7 @@ public class MainController implements Initializable{
 
         translateBtn.setOnAction(event -> showComponent("/view/Translate.fxml"));
 
-        wikiBtn.setOnAction(event -> showComponent("/view/Wiki.fxml"));
+        editBtn.setOnAction(event -> showComponent("/view/Edit.fxml"));
 
         showComponent("/view/Search.fxml");
 
--- src/main/java/dictionary/controllers/WikiController.java
+++ src/main/java/dictionary/controllers/WikiController.java
@@ -2,17 +2,21 @@ package dictionary.controllers;
 
 import dictionary.services.WikFetchService;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class WikiController {
+import java.net.URL;
+import java.util.ResourceBundle;
+public class WikiController implements Initializable {
     @FXML
     private TextField searchBox = new TextField();
     @FXML
     private TextArea renderedText = new TextArea();
     @FXML
     private Button search;
+
     @FXML
     public void handleSearch() {
         String word = searchBox.getText();
@@ -21,4 +25,9 @@ public class WikiController {
         WikFetchService wfService = new WikFetchService();
         renderedText.setText(wfService.search(word));
     }
+    @FXML
+    public void initialize(URL location, ResourceBundle Resources) {
+
+    }
+
 }
