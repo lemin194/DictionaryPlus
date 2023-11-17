ade3672ec286cf95def40950bc20c19a35a369c8
lemin194 <leminhduc190204@gmail.com>
2023-11-17 22:57:47 +0700

    Add StringUtils.java

---
+++ src/main/java/dictionary/services/StringUtils.java
@@ -0,0 +1,21 @@
+package dictionary.services;
+
+import dictionary.models.Entity.Word;
+import java.util.List;
+
+public class StringUtils {
+  public static String getFirstMeaning(Word w) {
+    String meaning = w.getMeaning();
+    List<String> meanings = List.of(meaning.split("\n"));
+    for (String m : meanings) {
+      m = m.trim().replace("- ", "");
+      if (m.isEmpty()) {
+        continue;
+      }
+      if (m.startsWith("(viết tắt)")) continue;
+//      System.out.println(m);
+      return m;
+    }
+    return meanings.get(0).trim().replace("- ", "");
+  }
+}
